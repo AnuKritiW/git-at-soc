af64535191b19385e252fdbed26886d53fcf042d
Melodies <melodiessim98@gmail.com>
2020-04-17 19:06:52 +0800

    ps6 happytreat

--- Happytreat.py
+++ Happytreat.py
@@ -7,7 +7,7 @@ class Solution(object):
         :type wordDict: List[str]
         :rtype: bool
         """
-        # 16ms faster than 98.32% of Python
+        # 12ms faster than 99.80% of Python
         # time: O(s^2 + n)
         # space: O(n + s)
         if not wordDict: 
@@ -20,14 +20,17 @@ class Solution(object):
             wordSet.add(word)
             max_len = max(max_len, len(word))
             min_len = min(min_len, len(word))
-            
+        
+        if min_len > len(s): 
+            return false
+        
         dp = defaultdict(bool)
         dp[len(s)] = True
         
         for i in range(len(s)-1, -1, -1):
             if not dp[i+1]:
                 continue
-            max_j = i - min_len + 1
+            max_j = max(i - min_len + 1, -1)
             min_j = max(i - max_len, -1)
             for j in range(max_j, min_j, -1):
                 if s[j:i+1] in wordSet:
@@ -35,3 +38,5 @@ class Solution(object):
                     if j == 0:
                         return True
         return False
+        
+                
